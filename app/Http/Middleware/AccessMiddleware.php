48225db422c8ca5a83846c4e2e2ff2e8f63c947e
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-12-29 15:09:51 +0530

    fix

--- app/Http/Middleware/AccessMiddleware.php
+++ app/Http/Middleware/AccessMiddleware.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use App\Http\Middleware\Auth;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AccessMiddleware
