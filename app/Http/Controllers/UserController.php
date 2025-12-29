feb2117243cef44bce45ca2a7ba7f5e600476b1e
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-12-29 15:01:30 +0530

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,7 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Traits\ApiResponse;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
