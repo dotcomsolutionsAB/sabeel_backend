b8dfd095df7db9da0836e46bbf4d9720debeedda
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2026-01-06 15:52:47 +0530

    fix

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Schema;
 use App\Helpers\ExcelExportHelper;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\GenericExcelExport;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
