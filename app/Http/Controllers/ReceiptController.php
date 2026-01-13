a93850f40ce82c80b00a34910756df993d002b88
Burhanuddin Kanchwala <dotcomsolutiononline@gmail.com>
2026-01-13 09:22:58 +0530

    ..

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -6,6 +6,7 @@ use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
 use App\Helpers\ExcelExportHelper;
 use Illuminate\Support\Facades\DB;
